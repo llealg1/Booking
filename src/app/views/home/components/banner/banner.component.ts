f8cee43514ce797362a9c1473e7bc3cb19a9cdf9
Felix Cicilia <felixcicilia@MacBook-Air-de-Felix.local>
2024-12-31 11:54:12 -0400

    traia de cambios de luij

---
+++ src/app/views/home/components/banner/banner.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core'
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
+
+@Component({
+  selector: 'offer-detail-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner.component.html',
+  styles: ``,
+})
+export class BannerComponent {
+
+}
