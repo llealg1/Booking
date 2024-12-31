b09f3ddc5cc7f538b9c1e2465bd4c090ab0b7e8a
Felix Cicilia <felixcicilia@MacBook-Air-de-Felix.local>
2024-12-31 13:24:54 -0400

    traia de cambios

--- src/app/views/home/principal/principal.component.ts
+++ src/app/views/home/principal/principal.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, signal, TemplateRef, ViewChild, WritableSignal } from '@angular/core';
 import { TopbarComponent } from '../components/topbar/topbar.component';
 import { TestimonialSliderComponent } from '../components/testimonial-slider/testimonial-slider.component';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { ActionBoxComponent } from '../components/action-box/action-box.component';
 import { HeroComponent } from '../components/hero/hero.component';
+import { Footer1Component } from '../../about-us/about/components/footer1/footer1.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { BannerComponent } from '../components/banner/banner.component';
+import {
+  NgbModalModule,
+  NgbNavModule,
+  NgbModal,
+  ModalDismissReasons
+} from '@ng-bootstrap/ng-bootstrap'
+import { FeaturedHolidayComponent } from '../components/featured-holiday/featured-holiday.component';
+import { TeamListComponent } from '../components/team-list/team-list.component';
 @Component({
   selector: 'app-principal',
   standalone: true,
@@ -12,12 +23,56 @@ import { HeroComponent } from '../components/hero/hero.component';
     TestimonialSliderComponent,
     NgbAlertModule,
     ActionBoxComponent,
-    HeroComponent
+    HeroComponent,
+    FooterComponent,
+    NgbModalModule,
+    NgbNavModule,
+    BannerComponent,
+    FeaturedHolidayComponent,
+    TeamListComponent
   ],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
-export class PrincipalComponent {
+export class PrincipalComponent implements OnInit, AfterViewInit {
   staticAlert = true
   promoAlert = true
+
+
+  private modalService = inject(NgbModal);
+	closeResult: WritableSignal<string> = signal('');
+  @ViewChild('content') modalBanner!: TemplateRef<any>;
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    // this.open(this.modalBanner);
+  }
+
+  ngAfterViewInit(): void {
+    this.open(this.modalBanner);
+  }
+
+  open(content: TemplateRef<any>) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg', centered: true }).result.then(
+      (result) => {
+        this.closeResult.set(`Closed with: ${result}`);
+      },
+      (reason) => {
+        this.closeResult.set(`Dismissed ${this.getDismissReason(reason)}`);
+      }
+    );
+  }
+  private getDismissReason(reason: any): string {
+		switch (reason) {
+			case ModalDismissReasons.ESC:
+				return 'by pressing ESC';
+			case ModalDismissReasons.BACKDROP_CLICK:
+				return 'by clicking on a backdrop';
+			default:
+				return `with: ${reason}`;
+		}
+	}
+
 }
