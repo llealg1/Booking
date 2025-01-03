1ee24dff206d18230f0b1c733b2e8c3778b5a52a
Felix Cicilia <felixcicilia@MacBook-Air-de-Felix.local>
2025-01-03 17:58:07 -0400

    cambios en iniciar sesion, registro, olvido de contrasena y parte del sidear del profile

--- src/app/views/user/profile/profile.component.ts
+++ src/app/views/user/profile/profile.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core'
 import { ProgressBarComponent } from './component/progress-bar/progress-bar.component'
-import { UpdateEmailComponent } from './component/update-email/update-email.component'
 import { UpdatePasswordComponent } from './component/update-password/update-password.component'
+import { PersonalInfoComponent } from './component/personal-info/personal-info.component'
+import { EmergencyContactComponent } from './component/emergency-contact/emergency-contact.component'
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
     ProgressBarComponent,
-    UpdateEmailComponent,
     UpdatePasswordComponent,
+    PersonalInfoComponent,
+    EmergencyContactComponent,
   ],
   templateUrl: './profile.component.html',
   styles: ``,
