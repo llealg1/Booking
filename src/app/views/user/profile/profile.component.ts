484cf0f95aa7b8069e41a535c1cc4cccff11b3d8
Luis Leal <luislealgalicia@gmail.com>
2025-01-03 00:17:58 -0400

    fix: felix la cagaste

--- src/app/views/user/profile/profile.component.ts
+++ src/app/views/user/profile/profile.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core'
 import { ProgressBarComponent } from './component/progress-bar/progress-bar.component'
-import { PesronalInfoComponent } from './component/pesronal-info/pesronal-info.component'
 import { UpdateEmailComponent } from './component/update-email/update-email.component'
 import { UpdatePasswordComponent } from './component/update-password/update-password.component'
 
@@ -9,7 +8,6 @@ import { UpdatePasswordComponent } from './component/update-password/update-pass
   standalone: true,
   imports: [
     ProgressBarComponent,
-    PesronalInfoComponent,
     UpdateEmailComponent,
     UpdatePasswordComponent,
   ],
