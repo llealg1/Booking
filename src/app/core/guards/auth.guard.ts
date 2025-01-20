72e36ba39c82fa1ccf90f160e67d2a11f35089c5
llealg1 <luislealgalicia@gmail.com>
2025-01-19 22:52:08 -0400

    arreglado auth del token si esta vencido

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,17 +1,33 @@
-import { inject } from '@angular/core'
+import { inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivateFn,
   RouterStateSnapshot,
-} from '@angular/router'
-import { AuthenticationService } from '@core/services/auth.service'
+  Router,
+} from '@angular/router';
+import { AuthenticationService } from '@core/services/auth.service';
+import { Observable, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 export const AuthGuard: CanActivateFn = (
   next: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
-) => {
-  const authenticationService = inject(AuthenticationService)
-  const currentUser = authenticationService.session
-  if (currentUser) return true
-  return false
-}
+): Observable<boolean> => {
+  const authenticationService = inject(AuthenticationService);
+  const router = inject(Router);
+
+  return authenticationService.authMe().pipe(
+    map((user) => {
+      if (user) {
+        return true;
+      } else {
+        router.navigate(['/auth/sign-in']);
+        return false;
+      }
+    }),
+    catchError(() => {
+      router.navigate(['/auth/sign-in']);
+      return of(false);
+    })
+  );
+};
