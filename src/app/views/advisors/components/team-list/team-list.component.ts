a550d2c5d4abe3939aefbffb7e91b4be8a214aa3
llealg1 <luislealgalicia@gmail.com>
2025-02-13 13:27:21 -0400

    feat:

--- src/app/views/advisors/components/team-list/team-list.component.ts
+++ src/app/views/advisors/components/team-list/team-list.component.ts
@@ -17,14 +17,31 @@ export class TeamListComponent implements OnInit {
   ]
 
   constructor(private consultantsService: ConsultantsService) {
-    this.consultantsService.getUsersPortal().subscribe((consultants) => {
-      // console.log(consultants)
-      this.teamMembers = consultants.data
-    })
+
   }
   ngOnInit() {
-    // console.log('TeamListComponent initialized')
+    const startDate = this.getFirstDayOfMonth();
+    const endDate = this.getLastDayOfMonth();
+    this.consultantsService.getUsersPortalHome(startDate, endDate).subscribe((consultants) => {
+      console.log(consultants)
+      this.teamMembers = consultants
+    })
   }
+  getFirstDayOfMonth(): string {
+    const date = new Date();
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    firstDay.setUTCHours(0, 0, 0, 0);
+    return firstDay.toISOString();
+  }
+
+  getLastDayOfMonth(): string {
+    const date = new Date();
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    lastDay.setUTCHours(23, 59, 59, 999);
+    return lastDay.toISOString();
+  }
+
+
 
 
 }
