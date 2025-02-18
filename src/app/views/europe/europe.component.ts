c36100c04ddc1f4d135224e35e35c0dee3354d87
llealg1 <luislealgalicia@gmail.com>
2025-02-18 19:03:16 -0400

    ti

--- src/app/views/europe/europe.component.ts
+++ src/app/views/europe/europe.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { UntypedFormGroup, UntypedFormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-europe',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './europe.component.html',
   styleUrl: './europe.component.scss'
 })
 export class EuropeComponent {
+  contactForm!: UntypedFormGroup
+  submitted: boolean = false
 
+  private fb = inject(UntypedFormBuilder)
+
+  constructor() {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+    })
+  }
+
+  get form() {
+    return this.contactForm.controls
+  }
+
+  onSubmit() {
+    this.submitted = true
+  }
 }
