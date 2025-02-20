53c8c19d25fd2c5088cb454c61662c6436b180fe
flch3110 <flch3110@gmail.com>
2025-02-20 17:53:10 -0400

    cambios de landing para ads por fiiiiin

--- src/app/core/services/contact.service.ts
+++ src/app/core/services/contact.service.ts
@@ -1,30 +1,30 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
-  private baseUrl: string = 'https://arcadia-viajes.com:3000';
-  private apiKey: string = 'apikey2025';
+  private baseUrl: string = 'https://arcadia-viajes.com:3000'
+  private apiKey: string = 'apikey2025'
 
   constructor(private http: HttpClient) {}
 
   sendContactForm(contactData: {
-    lastName: string;
-    name: string;
-    email: string;
-    phone: string;
-    description: string;
-    subject: string;
+    lastName: string
+    name: string
+    email: string
+    phone: string
+    description: string
+    subject: string
   }): Observable<any> {
-    const url = `${this.baseUrl}/contact`;
+    const url = `${this.baseUrl}/contact`
     return this.http.post(url, contactData, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${this.apiKey}`
-      }
-    });
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+    })
   }
 }
